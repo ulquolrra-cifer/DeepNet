c9ac98a64ecba70b8c83b5a6dbf429842baad018
ubuntu733 <ubuntu733@gmail.com>
2015-05-05 22:37:11 +0800

    change use w and b

--- sda.py
+++ sda.py
@@ -1,5 +1,5 @@
 import numpy as np
-from nn import *
+from nn2 import *
 
 class Sda(object):
 	def __init__(self,architecture):
@@ -10,10 +10,11 @@ class Sda(object):
 			sub_architecture = [self.size[i-1],self.size[i],self.size[i-1]]
 #			self.da[str(i)] = NN(sub_architecture,architecture,activation_function,learningRate,output,scaling_learningRate)
 			self.da[str(i)] = NN(sub_architecture)
-	def train(self,x):
+	def train(self,x,batchsize,numepochs):
 		for i in range(1,self.n):
-			self.da[str(i)].nntrain(x,x,100,100)
+			self.da[str(i)].nntrain(x,x,batchsize,numepochs)
+	#		print i
 			self.da[str(i)].nnff(x,np.zeros((np.shape(x)[0],self.da[str(i)].size[-1])))
 			t = self.da[str(i)].a['2']
-			x = t[:,1:]
+			x = t.copy()
 		
