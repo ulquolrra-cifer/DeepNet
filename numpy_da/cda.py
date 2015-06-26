a145cdbb08656b014df8048abee0a557af9df2fb
ubuntu733 <ubuntu733@gmail.com>
2015-06-26 21:58:23 +0800

    delete

--- numpy_da/cda.py
+++
@@ -1,8 +0,0 @@
-#This is the implement for contractive auto-encoder
-import numpy 
-import nn
-class Cae(object):
-	def _init_(self,architecture):
-		self.size = architecture
-		self.n = len(architecture)
-		
