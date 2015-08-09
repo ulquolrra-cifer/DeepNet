54ce0953c1b1e4b02e841496b57ce17b5da66a97
ubuntu733 <ubuntu733@gmail.com>
2015-08-10 01:17:03 +0800

    add LSTM and GRU nets

--- network/theano_rnn/GRU.py
+++ network/theano_rnn/GRU.py
@@ -1,16 +1,9 @@
-"""
-Vanilla Recurrent Neural Network
-Thank to gwtaylor for his codes available at: 
-https://github.com/gwtaylor/theano-rnn
-
-"""
-import numpy as np
 import theano.tensor as T
 import theano
+import numpy as np
 import time
 from collections import OrderedDict
-
-class RNN(object):
+class GRU(object):
     def __init__(self,n_in,hidden,n_out,activation,output_type,error_type,learn_rate,moment,L1_reg,L2_reg):
         self.n_in = n_in
         self.hidden = hidden
@@ -24,40 +17,32 @@ class RNN(object):
         self.L2_reg = L2_reg
         self.x = T.matrix('x')
         self.y = T.matrix('y')
-        self.w_ih = theano.shared(np.asarray(np.random.uniform(
-                                                    size = (n_in, hidden),
-                                                    low = -.01, high = .01),
-                                                dtype = theano.config.floatX),
-                                  name = 'w_ih')
-
-        self.w_hh = theano.shared(np.asarray(np.random.uniform(
-                                                        size = (hidden, hidden),
-                                                        low = -.01, high = .01),
-                                                    dtype = theano.config.floatX),
-                                  name = 'w_hh')
-
+        #gate params
+        self.wz = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,n_in)),dtype=theano.config.floatX))
+        self.uz = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,hidden)),dtype=theano.config.floatX))
+        #reset params
+        self.wr = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,n_in)),dtype=theano.config.floatX))
+        self.ur = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,hidden)),dtype=theano.config.floatX))
+        #
+        self.w = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,n_in)),dtype=theano.config.floatX))
+        self.u = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,hidden)),dtype=theano.config.floatX))
         self.w_ho = theano.shared(np.asarray(np.random.uniform(
                                                     size = (hidden, n_out),
                                                     low = -.01, high = .01),
                                                 dtype = theano.config.floatX),
                                   name = 'w_ho')
-
-        # initial value of hidden layer units are set to zero
+        self.b_o = theano.shared(np.zeros((n_out, ),
+                                            dtype = theano.config.floatX),
+                                 name = 'b_o')        
         self.h0 = theano.shared(np.zeros((hidden, ),
                                             dtype = theano.config.floatX),
                                 name = 'h0')
 
-        # biases are initialized to zeros
-        self.b_h = theano.shared(np.zeros((hidden, ),
-                                             dtype = theano.config.floatX),
-                                 name = 'b_h')
-
-        self.b_o = theano.shared(np.zeros((n_out, ),
-                                            dtype = theano.config.floatX),
-                                 name = 'b_o')
-
-        self.params = [self.w_ih, self.w_hh, self.w_ho, self.h0,
-                       self.b_h, self.b_o]        
+                
+        self.b_z = theano.shared(np.zeros((hidden,),dtype=theano.config.floatX))
+        self.b_r = theano.shared(np.zeros((hidden,),dtype=theano.config.floatX))
+        self.b = theano.shared(np.zeros((hidden,),dtype=theano.config.floatX))
+        self.params = [self.wz,self.uz,self.wr,self.ur,self.w,self.u,self.b_z,self.b_r,self.b,self.w_ho,self.b_o,self.h0]
         self.updates = OrderedDict()
         for param in self.params:
             self.updates[param] = theano.shared(value = np.zeros(
@@ -66,28 +51,19 @@ class RNN(object):
                                                         dtype = theano.config.floatX),
                                                 name = 'updates')
         def step(u_t,h_tm1):
-            if self.activation == 'sigmoid':
-                h_t = T.nnet.sigmoid(T.dot(u_t,self.w_ih)+T.dot(h_tm1,self.w_hh)+self.b_h)
-                y_t = T.dot(h_t,self.w_ho) + self.b_o
-            elif self.activation == 'tanh':
-                h_t = T.tanh(T.dot(u_t,self.w_ih)+T.dot(h_tm1,self.w_hh)+self.b_h)
-                y_t = T.dot(h_t,self.w_ho) + self.b_o
-            elif self.activation == 'relu':
-                h_t = T.maximun((T.dot(u_t,self.w_ih)+T.dot(h_tm1,self.w_hh)+self.b_h),0)
-                y_t = T.dot(h_t,self.w_ho) + self.b_o
-            else:
-                raise NotImplementedError 
+            h_t = self.activation_function(u_t, h_tm1)
+#            c_t = h_t
+            y_t = T.dot(h_t,self.w_ho) + self.b_o
             return h_t,y_t
         [self.h,self.y_pred], _ = theano.scan(step,self.x,[self.h0,None])
-        self.L1 = abs(self.w_ih.sum()) + abs(self.w_hh.sum()) + abs(self.w_ho.sum())
-        self.L2 = ((self.w_ih) **2 ).sum() + ((self.w_hh) **2 ).sum() + ((self.w_ho) **2 ).sum()
+#        self.L1 = abs(self.w_ih.sum()) + abs(self.w_hh.sum()) + abs(self.w_ho.sum())
+#        self.L2 = ((self.w_ih) **2 ).sum() + ((self.w_hh) **2 ).sum() + ((self.w_ho) **2 ).sum()        
         if self.output_type == 'softmax':
             self.predicts = T.nnet.softmax(self.y_pred)
         elif self.output_type == 'sigmoid':
             self.predicts = T.nnet.sigmoid(self.y_pred)
         else:
             self.predicts = self.y_pred
-
 
         if self.error_type == 'mse':
             self.cost = T.mean((self.predicts - self.y) ** 2)
@@ -97,12 +73,18 @@ class RNN(object):
             self.cost = -T.mean(T.log(self.predicts)[T.arange(y.shape[0]), y])
         else:
                 raise NotImplementedError 
+    def activation_function(self,x_t,h_tm1):
+        zt = T.nnet.sigmoid(T.dot(self.wz,x_t)+T.dot(self.uz,h_tm1)+self.b_z)
+        rt = T.nnet.sigmoid(T.dot(self.wr,x_t)+T.dot(self.ur,h_tm1)+self.b_r)
+        h_temp = T.tanh(T.dot(self.w,x_t)+T.dot(self.u,h_tm1) * rt + self.b)
+        h_t = (1-zt) * h_tm1 + zt * h_temp
+        return h_t
 
     def train(self,train_x,train_y,test_x = None,test_y = None,epochs = 10):
         train_x = theano.shared(np.asarray(train_x, dtype=theano.config.floatX))
         train_y = theano.shared(np.asarray(train_y, dtype=theano.config.floatX))
         index = T.lscalar('index')
-        cost = self.cost + self.L1 * self.L1_reg + self.L2 * self.L2_reg
+        cost = self.cost 
         gparams =[]
         for param in self.params:
             gparam=T.grad(cost,param)
@@ -154,7 +136,7 @@ def example():
 
     targets += 0.01 * np.random.standard_normal(targets.shape)
 
-    myrnn = RNN(n_in = n_in, hidden = hidden, n_out=n_out,activation = 'tanh', output_type = 'real',error_type = 'mse',learn_rate = 0.001, L1_reg = 0, L2_reg = 0, moment = 0.9)
+    myrnn = GRU(n_in = n_in, hidden = hidden, n_out=n_out,activation = 'tanh', output_type = 'real',error_type = 'mse',learn_rate = 0.001, L1_reg = 0, L2_reg = 0, moment = 0.9)
     myrnn.train(seq, targets) 
 
 
@@ -174,4 +156,18 @@ if __name__ == '__main__':
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
 
--- network/theano_rnn/LSTM.py
+++ network/theano_rnn/LSTM.py
@@ -1,16 +1,9 @@
-"""
-Vanilla Recurrent Neural Network
-Thank to gwtaylor for his codes available at: 
-https://github.com/gwtaylor/theano-rnn
-
-"""
-import numpy as np
-import theano.tensor as T
 import theano
+import theano.tensor as T
+import numpy as np
 import time
 from collections import OrderedDict
-
-class RNN(object):
+class LSTM(object):
     def __init__(self,n_in,hidden,n_out,activation,output_type,error_type,learn_rate,moment,L1_reg,L2_reg):
         self.n_in = n_in
         self.hidden = hidden
@@ -24,40 +17,37 @@ class RNN(object):
         self.L2_reg = L2_reg
         self.x = T.matrix('x')
         self.y = T.matrix('y')
-        self.w_ih = theano.shared(np.asarray(np.random.uniform(
-                                                    size = (n_in, hidden),
-                                                    low = -.01, high = .01),
-                                                dtype = theano.config.floatX),
-                                  name = 'w_ih')
-
-        self.w_hh = theano.shared(np.asarray(np.random.uniform(
-                                                        size = (hidden, hidden),
-                                                        low = -.01, high = .01),
-                                                    dtype = theano.config.floatX),
-                                  name = 'w_hh')
-
+        #forgry params
+        self.wf = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,n_in)),dtype=theano.config.floatX))
+        self.uf = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,hidden)),dtype=theano.config.floatX))
+        #
+        self.wc = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,n_in)),dtype=theano.config.floatX))
+        self.uc = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,hidden)),dtype=theano.config.floatX))
+        #
+        self.w = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,n_in)),dtype=theano.config.floatX))
+        self.u = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,hidden)),dtype=theano.config.floatX))
+        self.wo = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,n_in)),dtype=theano.config.floatX))
+        self.uo = theano.shared(np.asarray(np.random.uniform(low=-0.01,high=0.01,size=(hidden,hidden)),dtype=theano.config.floatX))
         self.w_ho = theano.shared(np.asarray(np.random.uniform(
                                                     size = (hidden, n_out),
                                                     low = -.01, high = .01),
                                                 dtype = theano.config.floatX),
                                   name = 'w_ho')
-
-        # initial value of hidden layer units are set to zero
+        self.b_ho = theano.shared(np.zeros((n_out, ),
+                                            dtype = theano.config.floatX),
+                                 name = 'b_ho')        
         self.h0 = theano.shared(np.zeros((hidden, ),
                                             dtype = theano.config.floatX),
                                 name = 'h0')
-
-        # biases are initialized to zeros
-        self.b_h = theano.shared(np.zeros((hidden, ),
-                                             dtype = theano.config.floatX),
-                                 name = 'b_h')
-
-        self.b_o = theano.shared(np.zeros((n_out, ),
+        self.c0 = theano.shared(np.zeros((hidden, ),
                                             dtype = theano.config.floatX),
-                                 name = 'b_o')
+                                name = 'c0')
 
-        self.params = [self.w_ih, self.w_hh, self.w_ho, self.h0,
-                       self.b_h, self.b_o]        
+        self.b_o = theano.shared(np.zeros((hidden,),dtype=theano.config.floatX))       
+        self.b_f = theano.shared(np.zeros((hidden,),dtype=theano.config.floatX))
+        self.b_c = theano.shared(np.zeros((hidden,),dtype=theano.config.floatX))
+        self.b = theano.shared(np.zeros((hidden,),dtype=theano.config.floatX))
+        self.params = [self.wf,self.uf,self.wc,self.uc,self.w,self.u,self.b_f,self.b_c,self.b,self.w_ho,self.b_o,self.h0]
         self.updates = OrderedDict()
         for param in self.params:
             self.updates[param] = theano.shared(value = np.zeros(
@@ -65,29 +55,20 @@ class RNN(object):
                                                         borrow = True).shape,
                                                         dtype = theano.config.floatX),
                                                 name = 'updates')
-        def step(u_t,h_tm1):
-            if self.activation == 'sigmoid':
-                h_t = T.nnet.sigmoid(T.dot(u_t,self.w_ih)+T.dot(h_tm1,self.w_hh)+self.b_h)
-                y_t = T.dot(h_t,self.w_ho) + self.b_o
-            elif self.activation == 'tanh':
-                h_t = T.tanh(T.dot(u_t,self.w_ih)+T.dot(h_tm1,self.w_hh)+self.b_h)
-                y_t = T.dot(h_t,self.w_ho) + self.b_o
-            elif self.activation == 'relu':
-                h_t = T.maximun((T.dot(u_t,self.w_ih)+T.dot(h_tm1,self.w_hh)+self.b_h),0)
-                y_t = T.dot(h_t,self.w_ho) + self.b_o
-            else:
-                raise NotImplementedError 
-            return h_t,y_t
-        [self.h,self.y_pred], _ = theano.scan(step,self.x,[self.h0,None])
-        self.L1 = abs(self.w_ih.sum()) + abs(self.w_hh.sum()) + abs(self.w_ho.sum())
-        self.L2 = ((self.w_ih) **2 ).sum() + ((self.w_hh) **2 ).sum() + ((self.w_ho) **2 ).sum()
+        def step(u_t,h_tm1,c_tm1):
+            h_t,c_t = self.activation_function(u_t, h_tm1,c_tm1)
+#            c_t = h_t
+            y_t = T.dot(h_t,self.w_ho) + self.b_ho
+            return h_t,c_t,y_t
+        [self.h,self.c,self.y_pred], _ = theano.scan(step,self.x,[self.h0,self.c0,None])
+#        self.L1 = abs(self.w_ih.sum()) + abs(self.w_hh.sum()) + abs(self.w_ho.sum())
+#        self.L2 = ((self.w_ih) **2 ).sum() + ((self.w_hh) **2 ).sum() + ((self.w_ho) **2 ).sum()        
         if self.output_type == 'softmax':
             self.predicts = T.nnet.softmax(self.y_pred)
         elif self.output_type == 'sigmoid':
             self.predicts = T.nnet.sigmoid(self.y_pred)
         else:
             self.predicts = self.y_pred
-
 
         if self.error_type == 'mse':
             self.cost = T.mean((self.predicts - self.y) ** 2)
@@ -97,12 +78,20 @@ class RNN(object):
             self.cost = -T.mean(T.log(self.predicts)[T.arange(y.shape[0]), y])
         else:
                 raise NotImplementedError 
+    def activation_function(self,x_t,h_tm1,c_tm1):
+        it = T.nnet.sigmoid(T.dot(self.w,x_t)+T.dot(self.u,h_tm1)+self.b)
+        ft = T.nnet.sigmoid(T.dot(self.wf,x_t)+T.dot(self.uf,h_tm1)+self.b_f)
+        ot = T.nnet.sigmoid(T.dot(self.wo,x_t)+T.dot(self.uo,h_tm1)+self.b_o)
+        c_temp = T.tanh(T.dot(self.wc,x_t)+T.dot(self.uc,h_tm1)+self.b_c)
+        ct = ft * c_tm1 + it * c_temp
+        ht = ot * T.tanh(ct)
+        return ht,ct
 
     def train(self,train_x,train_y,test_x = None,test_y = None,epochs = 10):
         train_x = theano.shared(np.asarray(train_x, dtype=theano.config.floatX))
         train_y = theano.shared(np.asarray(train_y, dtype=theano.config.floatX))
         index = T.lscalar('index')
-        cost = self.cost + self.L1 * self.L1_reg + self.L2 * self.L2_reg
+        cost = self.cost 
         gparams =[]
         for param in self.params:
             gparam=T.grad(cost,param)
@@ -131,10 +120,6 @@ class RNN(object):
             t2 = time.time()
             print '%d/%d epoch,takes %f secondes,errors is %f' % (epoch,epochs,(t2-t1),np.mean(errors))
             epoch += 1  
-
-
-
-
 def example():
     n_in = 3 # input vector size (not time at this point)
     hidden = 10 # hidden vector size
@@ -154,24 +139,9 @@ def example():
 
     targets += 0.01 * np.random.standard_normal(targets.shape)
 
-    myrnn = RNN(n_in = n_in, hidden = hidden, n_out=n_out,activation = 'tanh', output_type = 'real',error_type = 'mse',learn_rate = 0.001, L1_reg = 0, L2_reg = 0, moment = 0.9)
+    myrnn = LSTM(n_in = n_in, hidden = hidden, n_out=n_out,activation = 'tanh', output_type = 'real',error_type = 'mse',learn_rate = 0.001, L1_reg = 0, L2_reg = 0, moment = 0.9)
     myrnn.train(seq, targets) 
 
 
 if __name__ == '__main__':
     example()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
